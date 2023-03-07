7a17547f2d9528f0344ba8b60c8fae428913567b
Jose Mora <jdmds13@gmail.com>
2023-03-07 16:36:17 -0400

    Logica del buscaminas final

--- buscaminas.php
+++ buscaminas.php
@@ -3,25 +3,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>tabla</title>
+    <title>Buscaminas</title>
 </head>
 <body>
     <h1>BUSCAMINAS</h1>
     <?php 
-       
-            if(!isset($_SESSION["matriz"]))
+    $_SESSION["fila"]=10; $_SESSION["col"]=10;
+    $minasT=20;
+      
+            if(!isset($_SESSION["matriz"])||$_SESSION["victoria"])
             {
-                
                 $cont=0;
-                for($i=0;$i<10;$i++)
+                for($i=0;$i<$_SESSION["fila"];$i++)
                 {
-                    for($j=0;$j<10;$j++)
+                    for($j=0;$j<$_SESSION["col"];$j++)
                     {
                         $mat[$i][$j]=false;
                         $mina[$i][$j]=false;
                         $minaAlr[$i][$j]=0;
                         $nro[$i][$j]=$cont;
                         $cont++;
+                        $expan[$i][$j]=false;
+                        $bandera[$i][$j]=0;
                     }
                    
                 }
@@ -29,10 +32,88 @@
                 $_SESSION["numero"]=$nro;
                 $_SESSION["matriz"]=$mat;
                 $_SESSION["mina"]=$mina;
-                $_SESSION["primer"]=false;
+                $_SESSION["expandir"]=$expan;
+                $_SESSION["primer"]=$_SESSION["victoria"]=$_SESSION["derrota"]=false;
+                $_SESSION["bandera"]=$bandera;
             }
-        $_SESSION["numerosAlr"]=[];
+            $_SESSION["numerosAlr"]=[]; 
         
+        
+            if(isset($_GET["fil"])&&isset($_GET["col"])&&isset($_GET["band"]))
+            {
+                if($_GET["band"]==0)
+                {
+                    if($_SESSION["bandera"][$_GET["fil"]][$_GET["col"]]==0)
+                    {
+                        if(!$_SESSION["primer"])        //primer click
+                        {
+                            
+                            Alrededor($_GET["fil"],$_GET["col"],1);   //saber cuales hay alrrededor para no poner ninguna mina alrededor  
+                            for($i=0;$i<$minasT;$i++)
+                            {
+                                $bandP=true;
+                                $vectorMinas[$i]=rand(0,(($_SESSION["fila"]*$_SESSION["col"])-1));
+                                for($j=0;$j<$i;$j++)
+                                {  
+                                    if($vectorMinas[$i]==$vectorMinas[$j])
+                                    {
+                                            $j=$i;
+                                            $i--;
+                                            $bandP=false;
+                                    }
+                                }
+                                if($bandP)  
+                                {
+                                    for($k=0;$k<sizeof($_SESSION["numerosAlr"]);$k++)
+                                    {
+                                        if($vectorMinas[$i]==$_SESSION["numerosAlr"][$k])
+                                        {
+                                            $k=sizeof($_SESSION["numerosAlr"]);
+                                            $i--;
+                                        }
+                                    }
+                                }
+            
+                            }
+                            for($f=0;$f<$minasT;$f++)
+                            {
+                                for($i=0;$i<$_SESSION["fila"];$i++)
+                                {
+                                    for($j=0;$j<$_SESSION["col"];$j++)
+                                    {
+                                        if($_SESSION["numero"][$i][$j]==$vectorMinas[$f])
+                                        {
+                                            $_SESSION["mina"][$i][$j]=true;
+                                        }
+                                    }
+                                }
+                            } 
+                            for($i=0;$i<$_SESSION["fila"];$i++)
+                            {
+                                for($j=0;$j<$_SESSION["col"];$j++)
+                                {
+                                    Alrededor($i,$j,3);
+                                }
+                            }
+                            Alrededor($_GET["fil"],$_GET["col"],2);
+                            $_SESSION["primer"]=true;
+                        }else
+                        {
+                            Alrededor($_GET["fil"],$_GET["col"],2);
+                        }
+                    }    
+                    ganar();
+                }else
+                {
+                    if($_SESSION["bandera"][$_GET["fil"]][$_GET["col"]]==0)
+                    {
+                        $_SESSION["bandera"][$_GET["fil"]][$_GET["col"]]=1;
+                    }else{
+                        $_SESSION["bandera"][$_GET["fil"]][$_GET["col"]]=0;
+                    }
+                    
+                }  
+            }
         function Alrededor($f,$c,$opcion)
         {
             
@@ -41,18 +122,23 @@
             {
                     for($j=$c-1;$j<=$c+1;$j++)
                     {
-                       if(($i>=0&&$i<10)&&($j>=0&&$j<10))
+                       if(($i>=0&&$i<$_SESSION["fila"])&&($j>=0&&$j<$_SESSION["col"]))
                        {
                        
                             switch($opcion)
                             {
                                
-                                case 1: $_SESSION["numerosAlr"][$contador]=$_SESSION["numero"][$i][$j];     //Saber los numeros alrededor
+                                case 1: $_SESSION["numerosAlr"][$contador]=$_SESSION["numero"][$i][$j];  //echo"",$_SESSION["numerosAlr"][$contador],"<br></br>";    //Saber los numeros alrededor
                                 break;    
-                                case 2: if(!$_SESSION["primer"]){$_SESSION["matriz"][$i][$j]=true;}
-                                /*if($_SESSION["minaAlrededor"][$i][$j]==0&&($i!=$f||$j!=$c)&&$_SESSION["matriz"][$i][$j]){Alrededor($i,$j,2); echo "prueba"; } *////Destapar los numeros alrededor
+                                case 2: if(!$_SESSION["primer"]||$_SESSION["minaAlrededor"][$f][$c]==0){$_SESSION["matriz"][$i][$j]=true;}else{$_SESSION["matriz"][$f][$c]=true;}
+                                        if($_SESSION["minaAlrededor"][$i][$j]==0&&($i!=$f||$j!=$c)&&$_SESSION["matriz"][$i][$j]
+                                        &&!$_SESSION["expandir"][$i][$j])
+                                        {
+                                         $_SESSION["expandir"][$i][$j]=true; Alrededor($i,$j,2);
+                                        } //Destapar los numeros alrededor
                                 break;
-                                case 3: if($_SESSION["mina"][$i][$j]){$_SESSION["minaAlrededor"][$f][$c]++;} break;//minas alrededor
+                                case 3: if($_SESSION["mina"][$i][$j]){$_SESSION["minaAlrededor"][$f][$c]++;} 
+                                break;//minas alrededor
                             }
                             $contador++;
                        }
@@ -60,107 +146,101 @@
             }
             
         }
-        
-            if(isset($_GET["fil"])&&isset($_GET["col"]))
+        function  ganar()
+        {
+            $bandG=true;
+            for($i=0;$i<$_SESSION["fila"];$i++)
             {
-                if(!$_SESSION["primer"])
-                {
-                    
-                    Alrededor($_GET["fil"],$_GET["col"],1);     
-                    for($i=0;$i<20;$i++)
+                for($j=0;$j<$_SESSION["col"];$j++)
+                {   
+                    if(!((($_SESSION["matriz"][$i][$j]&&!$_SESSION["mina"][$i][$j])||(
+                    !$_SESSION["matriz"][$i][$j]&&$_SESSION["mina"][$i][$j]))&&$bandG))
                     {
-                        $vectorMinas[$i]=rand(0,99);
-                        for($j=0;$j<$i;$j++)
-                        { 
-                            for($k=0;$k<sizeof($_SESSION["numerosAlr"]);$k++)
-                            {
-                                if($vectorMinas[$i]==$vectorMinas[$j]||$vectorMinas[$i]==$_SESSION["numerosAlr"][$k])
-                                {
-                                    
-                                    $i--;
-                                    $j=0;
-                                    break;
-                                }
-                            }
-                               
-                        }
+                        $bandG=false;
+                        //echo "",$_SESSION["numero"][$i][$j],"<br></br>";
                     }
-                    for($f=0;$f<20;$f++)
-                    {
-                        for($i=0;$i<10;$i++)
-                        {
-                            for($j=0;$j<10;$j++)
-                            {
-                                if($_SESSION["numero"][$i][$j]==$vectorMinas[$f])
-                                {
-                                    $_SESSION["mina"][$i][$j]=true;
-                                }
-                            }
-                        }
-                    } 
-                    for($i=0;$i<10;$i++)
-                    {
-                        for($j=0;$j<10;$j++)
-                        {
-                            Alrededor($i,$j,3);
-                        }
-                    }
-                    Alrededor($_GET["fil"],$_GET["col"],2);
-                    $_SESSION["primer"]=true;
-                }else
-                {
-                    Alrededor($_GET["fil"],$_GET["col"],2);
                 }
-                
             }
+            if($bandG)
+            {
+                $_SESSION["victoria"]=true;
+            }
+       }
+    
+
+            
     ?>
     <table id="tabla"  cellpadding="0">
         <?php
            
-            for($i=0;$i<10;$i++)
+            for($i=0;$i<$_SESSION["fila"];$i++)
                 {
                     echo "<tr>";
-                    for($j=0;$j<10;$j++)
+                    for($j=0;$j<$_SESSION["col"];$j++)
                     {   
                         
                         if(!$_SESSION["matriz"][$i][$j])
                         {
-                            echo "<td><a onclick='selec(",$i,",",$j,");'><img src='assets/tapado.png' id='imagenfila",$i,"columna",$j,"'
-                            width='30px' heigth='30px'></a></td>";
+                            if($_SESSION["bandera"][$i][$j]==0)
+                            {
+                                echo "<td><a onmousedown='selec(",$i,",",$j,");'><img src='assets/tapado.png' id='imagenfila",$i,"columna",$j,"'
+                                width='30px' heigth='30px'></a></td>";
+                            }else
+                            {
+                                echo "<td><a onmousedown='selec(",$i,",",$j,");'><img src='assets/bandera.png' id='imagenfila",$i,"columna",$j,"'
+                                width='30px' heigth='30px'></a></td>";
+                            }
                         }else{
                             if(!$_SESSION["mina"][$i][$j])
-                            {
-                                echo"<td><a onclick='selec(",$i,",",$j,");'><img src='assets/",$_SESSION["minaAlrededor"][$i][$j],".png' id='imagenfila",$i,"columna",$j,"'
+                            { 
+                                echo"<td><a onmousedown='selec(",$i,",",$j,");'><img src='assets/",$_SESSION["minaAlrededor"][$i][$j],".png' id='imagenfila",$i,"columna",$j,"'
                                 width='30px' heigth='30px'></a></td>";
                             }else{
-                                echo"<td><a onclick='selec(",$i,",",$j,");'><img src='assets/mina.png' id='imagenfila",$i,"columna",$j,"'
+                                echo"<td><a><img src='assets/mina.png' id='imagenfila",$i,"columna",$j,"'
                                 width='30px' heigth='30px'></a></td>";
+
+                               $_SESSION["derrota"]=true;  
                             }         
-                        }  
+                        }
                     }
                     echo "</tr>";
-                }
+                }   
         ?>
     </table>
    
     <button onclick="reiniciar()">Reiniciar</button>
+
+    <?php
+         if($_SESSION["derrota"])
+         {
+             echo '<script language="javascript">window.addEventListener("load", function(event){alert("Perdiste");document.location="reiniciar.php";})</script>';
+         }
+         if($_SESSION["victoria"])
+         {
+             echo '<script language="javascript">window.addEventListener("load", function(event){alert("Eres un Crack");})</script>';
+         }
+    ?>
 </body>
 <script>
     function selec(f,c)
-    {
-        // alert("fila: "+f+" columna: "+c+" seleccionada");
-        document.location="buscaminas.php?fil="+f+"&col="+c;
+    { 
+        let aux=event.button
+        switch(aux)
+        {
+            case 0: document.location="buscaminas.php?fil="+f+"&col="+c+"&band=0"; break;
+            case 2: document.location="buscaminas.php?fil="+f+"&col="+c+"&band=1"; break;
+        }
     }
-    function perdiste(f,c)
-    {
-        alert("fila: "+f+" columna: "+c+" seleccionada");
-       
-    }
-    function reiniciar(f,c)
+    function reiniciar()
     {
         document.location="reiniciar.php";
        
     }
-        
+    function inhabilitar()
+    {
+        "prueba";
+        return false;
+    }
+    document.oncontextmenu=inhabilitar;
 </script>
 </html>
